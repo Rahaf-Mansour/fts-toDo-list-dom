1137ada833408eebf4c0ea5083755b15f67e67ee
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-08-29 16:38:31 +0300

    Add the global array and the declare the needed dom elements

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Global array to store the TODO list data
+let todoList = [];
+
+// DOM elements
+const addText = document.getElementById('add-text');
+const searchInput = document.getElementById('search');
+const tableBody = document.querySelector('.table-list tbody');
+const countElement = document.querySelector('.count');
