fc2f88db985f29ab4243b4d7ee9a39cac0c3d4f5
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-08-29 17:14:41 +0300

    Add the fetch data function from the API

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ const addText = document.getElementById('add-text');
 const searchInput = document.getElementById('search');
 const tableBody = document.querySelector('.table-list tbody');
 const countElement = document.querySelector('.count');
+
+// Function to fetch TODO data from the API
+async function fetchTodoData() {
+    try {
+      const response = await fetch('https://dummyjson.com/todos');
+      const data = await response.json();
+      todoList = [...data.todos.slice(0, 5)]; // Get the first 5 tasks from the API
+      saveToStorage();
+      renderTodoList();
+    } catch (error) {
+      console.error('Error fetching TODO data:', error);
+    }
+}
+
+  // Event listener to fetch TODO data on page load
+window.addEventListener('load', fetchTodoData);
+
